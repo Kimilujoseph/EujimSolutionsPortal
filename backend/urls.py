b1cf40a6c6c0c017bc4dd6d42827981962cdddae
timothy joseph <timothyjoseph8580@gmail.com>
2025-05-15 12:28:05 +0300

    made sure the server is running

--- backend/urls.py
+++ backend/urls.py
@@ -5,5 +5,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1',include("usermanagement.urls.api"))
+    path('api/v1',include("users.urls.api"))
 ]
