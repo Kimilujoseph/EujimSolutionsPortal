dfa0f1206d6a695409d5cf4485523674848b7fb1
timothy joseph <timothyjoseph8580@gmail.com>
2025-06-02 19:51:15 +0300

    added the skill info the analytics details

--- jobseeker/repository/recruiter_interaction.py
+++ jobseeker/repository/recruiter_interaction.py
@@ -2,28 +2,26 @@ from .base_repository import JobSeekerBaseRepository
 from recruiter.models import Recruiter, RecruiterDoc, RecruiterTracking
 from django.db.models import Count
 
-class RecruiterInteraction(JobSeekerBaseRepository):
-    def __init__(self,model: RecruiterTracking = None):
-        super().__init__(model)
-        #initalize with the RecruiterTracking model
-        self.model = model or RecruiterTracking
+class RecruiterInteraction(JobSeekerBaseRepository[RecruiterTracking]):
+    def __init__(self):
+        super().__init__(RecruiterTracking)
+       
 
     #filter jobseeker
     def get_status_counts_by_job_seeker(self, job_seeker_id: int):
-        return self.model.objects.filter(job_seeker_id=job_seeker_id) \
+        return self.filter(job_seeker_id=job_seeker_id) \
             .values('status') \
             .annotate(count=Count('id'))
 
     def recruiter_engagement(self, job_seeker_id: int):
-   
-       try:
-            recruiter_count = self.model.objects.filter(job_seeker_id=job_seeker_id) \
-            .values('recruiter') \
-            .distinct() \
-            .count()
+        try:
+            recruiter_count = self.filter(job_seeker_id=job_seeker_id) \
+                .values('recruiter') \
+                .distinct() \
+                .count()
 
-            recent_interactions = self.model.objects.filter(job_seeker_id=job_seeker_id).select_related('recruiter') \
-                .order_by('-createdAt')[:10]  
+            recent_interactions = self.filter(job_seeker_id=job_seeker_id).select_related('recruiter') \
+                .order_by('-createdAt')[:10]
             return {
                 'recruiter_count': recruiter_count,
                 'recent_interactions': [
@@ -35,10 +33,18 @@ class RecruiterInteraction(JobSeekerBaseRepository):
                     } for interaction in recent_interactions
                 ]
             }
-       except Exception as e:
-                print(f"Error fetching recruiter engagement for job seeker {job_seeker_id}: {e}")
-                return {
-                    'recruiter_count': 0,
-                    'recent_interactions': []
-                }
+        except Exception as e:
+            print(f"Error fetching recruiter engagement for job seeker")
+            return {
+                'recruiter_count': 0,
+                'recent_interactions': []
+            }
+        #get application status
+
+    def get_application_status(self,job_seeker_id:int):
+        try:
+            return self.filter(job_seeker_id=job_seeker_id).values('id','status')
+        except Exception as e:
+            raise Exception(f"Error fetching application status for job seeker")
             
+                
