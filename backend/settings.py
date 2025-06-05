e0fc636d92c22762d73c3cd68589307035d50dff
timothy joseph <timothyjoseph8580@gmail.com>
2025-06-05 10:52:57 +0300

    removed the .env file from being tracked

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     #3rd party
     'rest_framework',
     'rest_framework_simplejwt',
