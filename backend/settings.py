5433a9e36990b60e150077650087e60122770c63
MaryAkoth <maryakoth846@gmail.com>
2025-06-16 16:13:40 +0300

    mariawere

--- backend/settings.py
+++ backend/settings.py
@@ -22,10 +22,10 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'NAME': ('eujimsolution'),
+        'USER': ('root'),
+        'PASSWORD': (''),
+        'HOST': ('localhost'),
         'PORT': '3306',
     }
 }
