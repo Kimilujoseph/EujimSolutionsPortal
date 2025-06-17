b16d4c1f53ea577890516f5d64caa257d0c93f33
timothy joseph <timothyjoseph8580@gmail.com>
2025-06-17 12:51:57 +0300

    some setting.py conflicts solved

--- backend/settings.py
+++ backend/settings.py
@@ -22,13 +22,14 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': ('eujimSolution'),
-        'USER': ('root'),
-        'PASSWORD': ('seth2019'),
-        'HOST': ('localhost'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '3306',
     }
 }
+
 
 # Application definition
 
