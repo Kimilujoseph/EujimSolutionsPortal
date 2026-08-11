7361db5966860f08354b5f7637db9ee610d35c7d
kimilujoseph <timothyjoseph8580@gmail.com>
2026-08-11 19:50:18 +0300

    rewrite the gunicorn file to utilze few resources

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m6$!-cp09yb#wf&rup4yuzfs41e*hh@lq@$f*sj)$x6%(bdvnn'
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-secure-production-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
